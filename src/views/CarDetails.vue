74fea3b9d2630ef57f164ca967ebfb75e70692e9
Devin Mugglin <151910900+De27vin@users.noreply.github.com>
2025-08-19 10:24:44 +0200

    Add: CarDetails design

--- src/views/CarDetails.vue
+++ src/views/CarDetails.vue
@@ -1,19 +1,28 @@
 <template>
-  <div v-if="car" class="car-detail">
-    <button @click="$router.push('/')" class="back-btn">⬅ Return</button>
+  <div v-if="car" class="car-detail container">
+    <button @click="$router.push('/')" class="btn back-btn">⬅ Return</button>
 
-    <h1>{{ car.brand }} {{ car.model }}</h1>
-    <img :src="car.image" :alt="car.model" />
+    <div class="car-card-detail">
+      <div class="car-image-wrapper">
+        <img :src="car.image" :alt="car.model" class="car-image-detail" />
+      </div>
 
-    <ul class="car-specs">
-      <li><strong>Production year: </strong> {{ car.year }}</li>
-      <li><strong>Horsepower: </strong> {{ car.hp }} HP</li>
-    </ul>
+      <div class="car-info">
+        <h1 class="car-title">{{ car.brand }} {{ car.model }}</h1>
+
+        <ul class="car-specs">
+          <li><strong>Production year:</strong> {{ car.year }}</li>
+          <li><strong>Horsepower:</strong> {{ car.hp }} HP</li>
+        </ul>
+
+        <button class="btn favourite-btn">⭐ Add to Favourites</button>
+      </div>
+    </div>
   </div>
 
-  <div v-else class="not-found">
+  <div v-else class="not-found container">
     <p>Car not found!</p>
-    <button @click="$router.push('/')">Back to Homepage</button>
+    <button @click="$router.push('/')" class="btn">Back to Homepage</button>
   </div>
 </template>
 
@@ -21,7 +30,7 @@
 import carsCollection from '../assets/cars.json'
 
 export default {
-  name: 'carsCollection',
+  name: 'CarDetails',
   props: ['id'],
   data() {
     return {
@@ -33,7 +42,7 @@ export default {
 
     const carsWithImages = carsCollection.map((car) => ({
       ...car,
-      image: new URL(`../assets/cars/${car.image}`, import.meta.url).href,
+      image: new URL(`../assets/carPictures/${car.image}`, import.meta.url).href,
     }))
 
     this.car = carsWithImages.find((c) => c.id === carId)
