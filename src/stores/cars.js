f2e81f6785548a7cb8c2debf732ee829c2299e69
Devin Mugglin <151910900+De27vin@users.noreply.github.com>
2025-08-20 10:23:44 +0200

    Fix: Saving cars as favourites

--- src/stores/cars.js
+++ src/stores/cars.js
@@ -1,43 +1,79 @@
 import { defineStore } from 'pinia'
+import seed from '../assets/cars.json'
 
 export const useCarStore = defineStore('cars', {
   state: () => ({
     cars: [],
-    favourites: [],
+    favourites: [], // <-- nur IDs
   }),
+
+  getters: {
+    favouriteCars(state) {
+      return state.cars.filter((c) => state.favourites.includes(c.id))
+    },
+    isFavourite: (state) => (id) => state.favourites.includes(id),
+  },
+
   actions: {
+    ensureSeeded() {
+      if (this.cars.length === 0) {
+        this.cars = seed.map((car) => ({
+          ...car,
+          image: new URL(`../assets/carPictures/${car.image}`, import.meta.url).href,
+        }))
+      }
+    },
+
     addCar(car) {
-      this.cars.push(car)
+      const newId = this.cars.length ? Math.max(...this.cars.map((c) => c.id)) + 1 : 1
+      this.cars.push({ id: newId, ...car })
       this.saveToLocalStorage()
     },
-    deleteCar(id) {
-      this.cars = this.cars.filter((car) => car.id !== id)
-      this.saveToLocalStorage()
-    },
-    updateCar(updatedCar) {
-      const index = this.cars.findIndex((car) => car.id === updatedCar.id)
-      if (index !== -1) {
-        this.cars[index] = updatedCar
+
+    updateCar(updated) {
+      const idx = this.cars.findIndex((c) => c.id === updated.id)
+      if (idx !== -1) {
+        this.cars[idx] = { ...updated }
         this.saveToLocalStorage()
       }
     },
-    toggleFavourite(car) {
-      if (this.favourites.find((c) => c.id === car.id)) {
-        this.favourites = this.favourites.filter((c) => c.id !== car.id)
-      } else {
-        this.favourites.push(car)
-      }
+
+    deleteCar(id) {
+      this.cars = this.cars.filter((c) => c.id !== id)
+      this.favourites = this.favourites.filter((fid) => fid !== id) // aus Favs entfernen
       this.saveToLocalStorage()
     },
+
+    toggleFavourite(id) {
+      const set = new Set(this.favourites)
+      set.has(id) ? set.delete(id) : set.add(id)
+      this.favourites = Array.from(set) // de-duped
+      this.saveToLocalStorage()
+    },
+
+    loadFromLocalStorage() {
+      const sc = localStorage.getItem('cars')
+      const sf = localStorage.getItem('favourites')
+
+      if (sc) {
+        this.cars = JSON.parse(sc)
+      } else {
+        this.ensureSeeded()
+        this.saveToLocalStorage()
+      }
+
+      if (sf) {
+        let favs = JSON.parse(sf)
+        if (Array.isArray(favs) && favs.length && typeof favs[0] === 'object') {
+          favs = favs.map((x) => x.id)
+        }
+        this.favourites = favs
+      }
+    },
+
     saveToLocalStorage() {
       localStorage.setItem('cars', JSON.stringify(this.cars))
       localStorage.setItem('favourites', JSON.stringify(this.favourites))
-    },
-    loadFromLocalStorage() {
-      const savedCars = localStorage.getItem('cars')
-      const savedFavs = localStorage.getItem('favourites')
-      if (savedCars) this.cars = JSON.parse(savedCars)
-      if (savedFavs) this.favourites = JSON.parse(savedFavs)
     },
   },
 })
